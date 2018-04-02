ac01743d424585e272f0125fe14a7fb36d470fdb
nikhil133 <nikn135@gmail.com>
2018-04-02 07:22:09 +0530

    Comments added

--- source/eignwcpp.cpp
+++ source/eignwcpp.cpp
@@ -22,33 +22,33 @@ eignwcpp::eignwcpp(double **A,double **egvctr,double **eigval,int m,int n)
 void eignwcpp::eigen_mem_alloc(double **A,double **egvctr,double **eigval,int m,int n)
 {
 	
-	emxArray_real32_T *x;
+	emxArray_real32_T *x; //pointer object to emxArray_real32_T struct
 	emxArray_real32_T *y;
 	emxArray_real32_T *z;
 
-	emxInit_real32_T(&x,2);
+	emxInit_real32_T(&x,2);//Set dimension of matrix x
 	emxInit_real32_T(&y,2);
 	emxInit_real32_T(&z,2);
 
-	x->size[0]=m;
-	x->size[1]=n;
+	x->size[0]=m; //initializing row-memory for matrix x
+	x->size[1]=n; //initializing column-memory for matrix x
 	y->size[0]=m;
 	y->size[1]=n;
 	z->size[0]=m;
 	z->size[1]=n;
 
-	emxEnsureCapacity((emxArray__common *)x,0,(int32_T)sizeof(real_T));
+	emxEnsureCapacity((emxArray__common *)x,0,(int32_T)sizeof(real_T));//Memory is allocated by the CPU to matrix x
 	emxEnsureCapacity((emxArray__common *)y,0,(int32_T)sizeof(real_T));
 	emxEnsureCapacity((emxArray__common *)z,0,(int32_T)sizeof(real_T));
 	
 	for(int i=0;i<m;i++)
 	{
-		for(int j=0;j<n;j++)
+		for(int j=0;j<n;j++) 
 		{
-			x->data[i*n+j]=A[i][j];
+			x->data[i*n+j]=A[i][j];// copying of data from main to x matrix of real 32 bit format
 		}
 	}
-	geteigen(x,y,z);
+	geteigen(x,y,z); //calling function to compute eigen value and eigen vector, y matrix collects eigen vector and z collects eigen value.
 	for(int i=0;i<m;i++)
 	{
 		for(int j=0;j<n;j++)
